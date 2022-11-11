fab3edca31ed46ecf5ac9e80b879cf91dbf0cc2f
andrii.nechyporuk <andrii.nechyporuk@oa.edu.ua>
2022-11-11 06:56:49 +0200

    Search added

---
+++ KeysShop/KeysShop.UI/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using KeysShop.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using KeysShop.Core;
+using KeysShop.Repository;
+using System.Diagnostics;
+
+namespace KeysShop.UI.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly KeysRepository keysRepository;
+
+        public SearchController(KeysRepository keysRepository)
+        {
+            this.keysRepository = keysRepository;
+        }
+
+        public async Task<IActionResult> SearchKey(string keyname)
+        {
+            if(String.IsNullOrEmpty(keyname))
+            {
+                return RedirectToAction("SearchError");
+            }
+            var list = keysRepository.GetKeys();
+            list = list.Where(s => s.Name!.ToLower().Contains(keyname.ToLower())).ToList();
+
+            ViewBag.Keys = list;
+
+            return View("Index");
+        }
+
+        public IActionResult SearchError()
+        {
+            ViewBag.Message = "Пошукова стрічка не може бути пустою";
+            return View();
+        }
+    }
+}
